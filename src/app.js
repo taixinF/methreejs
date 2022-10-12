4dbb280c6e346673788960a33754b38fd306911d
与我 <1349458861@qq.com>
2022-10-12 16:08:01 +0800

    shadow 16 finish

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import './style.css'
 import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import {
+    OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
-import {RectAreaLightHelper} from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
+
+/*
+* texture
+* */
+const textureLoader = new THREE.TextureLoader()
+const backedShadow = textureLoader.load('../static/textures/bakedShadow.jpg')
+const simpleShadow = textureLoader.load('../static/textures/simpleShadow.jpg')
+
 
 /**
  * Base
@@ -19,167 +28,142 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-// AmbientLight
-// 环境光会均匀的照亮场景中的所有物体。
-// 环境光不能用来投射阴影，因为它没有方向。
-// 构造函数
-// AmbientLight( color : Integer, intensity : Float )
-// color - (参数可选）颜色的rgb数值。缺省值为 0xffffff。
-// intensity - (参数可选)光照的强度。缺省值为 1。
-// 创建一个环境光对象。
-// 属性
-// 公共属性请查看基类 Light。
-// .castShadow : Boolean
-// 这个参数在对象构造的时候就被设置成了 undefined 。因为环境光不能投射阴影。
-// .isAmbientLight : Boolean
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+// Ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
+ambientLight.castShadow = false
 
-// 平行光（DirectionalLight）
-// 平行光是沿着特定方向发射的光。这种光的表现像是无限远,从它发出的光线都是平行的。
-// 常常用平行光来模拟太阳光 的效果; 太阳足够远，因此我们可以认为太阳的位置是无限远，所以我们认为从太阳发出的光线也都是平行的。
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
-directionalLight.position.set(1, 0.25, 0)
-scene.add(directionalLight);
+// Directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3)
+directionalLight.position.set(2, 2, -1)
+gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
+gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001)
+gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001)
+gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001)
+scene.add(directionalLight)
+// .castShadow : Boolean
+// 如果设置为 true 该平行光会产生动态阴影。 警告: 这样做的代价比较高而且需要一直调整到阴影看起来正确.
+// 查看 DirectionalLightShadow 了解详细信息。该属性默认为 false。
+directionalLight.castShadow = false
+//     .shadow : DirectionalLightShadow
+// 这个 DirectionalLightShadow 对象用来计算该平行光产生的阴影。
+//     .mapSize : Vector2
+// 一个Vector2定义阴影贴图的宽度和高度。
+directionalLight.shadow.mapSize.x = 1024
+directionalLight.shadow.mapSize.y = 1024
+directionalLight.shadow.camera.near = 2
+directionalLight.shadow.camera.top = 2
+directionalLight.shadow.camera.right = 2
+directionalLight.shadow.camera.bottom = -2
+directionalLight.shadow.camera.left = -2
+directionalLight.shadow.camera.far = 6
+// .radius : Float
+// 将此值设置为大于1的值将模糊阴影的边缘。
+// 较高的值会在阴影中产生不必要的条带效果 - 更大的mapSize将允许在这些效果变得可见之前使用更高的值。
+// 请注意，如果[page：WebGLRenderer.shadowMap.type]设置为BasicShadowMap，将会无效。
+// directionalLight.shadow.radius = 10
 
-//半球光（HemisphereLight）
-// 光源直接放置于场景之上，光照颜色从天空光线颜色渐变到地面光线颜色。
-// 半球光不能投射阴影。
-// HemisphereLight( skyColor : Integer, groundColor : Integer, intensity : Float )
-// skyColor - (可选参数) 天空中发出光线的颜色。 缺省值 0xffffff。
-// groundColor - (可选参数) 地面发出光线的颜色。 缺省值 0xffffff。
-// intensity - (可选参数) 光照强度。 缺省值 1。
-const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.5);
-scene.add(hemisphereLight);
-
-// 点光源（PointLight）
-// 从一个点向各个方向发射的光源。一个常见的例子是模拟一个灯泡发出的光。
-// PointLight( color : Integer, intensity : Float, distance : Number, decay : Float )
-// color - (可选参数)) 十六进制光照颜色。 缺省值 0xffffff (白色)。
-// intensity - (可选参数) 光照强度。 缺省值 1。
-// distance - 这个距离表示从光源到光照强度为0的位置。 当设置为0时，光永远不会消失(距离无穷大)。缺省值 0.
-// decay - 沿着光照距离的衰退量。缺省值 1。 在 physically correct 模式中，decay = 2。
-const pointLight = new THREE.PointLight(0xffffff, 0.5, 10, 2)
-pointLight.position.set(1, -0.5, 1)
-scene.add(pointLight)
-
-// 平面光光源（RectAreaLight）
-// 平面光光源从一个矩形平面上均匀地发射光线。这种光源可以用来模拟像明亮的窗户或者条状灯光光源。
-// RectAreaLight( color : Integer, intensity : Float, width : Float, height : Float )
-// color - (可选参数) 十六进制数字表示的光照颜色。缺省值为 0xffffff (白色)
-// intensity - (可选参数) 光源强度／亮度 。缺省值为 1。
-// width - (可选参数) 光源宽度。缺省值为 10。
-// height - (可选参数) 光源高度。缺省值为 10。
-//搭配MeshPhongMaterial 材质使用效果更佳
-// Phong网格材质(MeshPhongMaterial)
-// 一种用于具有镜面高光的光泽表面的材质。
-// 该材质使用非物理的Blinn-Phong模型来计算反射率。
-// 与MeshLambertMaterial中使用的Lambertian模型不同，
-// 该材质可以模拟具有镜面高光的光泽表面（例如涂漆木材）。MeshPhongMaterial uses per-fragmet shading。
-const rectLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1)
-rectLight.position.set(-1.5, 0, 1.5)
-rectLight.lookAt(new THREE.Vector3())
-scene.add((rectLight))
-
-// 聚光灯（SpotLight）
-// 光线从一个点沿一个方向射出，随着光线照射的变远，光线圆锥体的尺寸也逐渐增大。
-// 构造器（Constructor）
-// SpotLight( color : Integer, intensity : Float, distance : Float, angle : Radians, penumbra : Float, decay : Float )
-// color - (可选参数) 十六进制光照颜色。 缺省值 0xffffff (白色)。
-// intensity - (可选参数) 光照强度。 缺省值 1。
-// distance - 从光源发出光的最大距离，其强度根据光源的距离线性衰减。
-// angle - 光线散射角度，最大为Math.PI/2。
-// penumbra - 聚光锥的半影衰减百分比。在0和1之间的值。默认为0。
-// decay - 沿着光照距离的衰减量。
-const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1)
-//光范围
-spotLight.position.set(0, 2, 3)
+//SpotLight
+const spotLight = new THREE.SpotLight(0xffffff, 0.3, 10, Math.PI * 0.3)
+spotLight.position.set(0, 2, 2)
+spotLight.castShadow = false
+spotLight.shadow.mapSize.width = 1024
+spotLight.shadow.mapSize.height = 1024
+spotLight.shadow.camera.fov = 30 //聚光宽度
+spotLight.shadow.camera.near = 1
+spotLight.shadow.camera.far = 6
 scene.add(spotLight)
-//光位置
-spotLight.target.position.x = -1
 scene.add(spotLight.target)
+const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera)
+spotLightCameraHelper.visible = false
+scene.add(spotLightCameraHelper)
 
-/*
-* helpers
-* */
-// 创建一个虚拟的球形网格 Mesh 的辅助对象来模拟 半球形光源 HemisphereLight.
-// HemisphereLightHelper( light : HemisphereLight, sphereSize : Number, color : Hex )
-// light -- 被模拟的光源.
-//     size -- 用于模拟光源的网格尺寸.
-//     color -- (可选的) 如果没有赋值辅助对象将使用光源的颜色.
-const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
-scene.add(hemisphereLightHelper)
+//pointLight
+const pointLight = new THREE.PointLight(0xffffff, 0.4)
+pointLight.castShadow = false
+pointLight.shadow.mapSize.width = 1024
+pointLight.shadow.mapSize.hieght = 1024
+pointLight.shadow.camera.near = 0.1
+pointLight.shadow.camera.far = 6
+pointLight.position.set(-1, 1, 0)
+scene.add(pointLight)
+const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera)
+pointLightCameraHelper.visible = false
+scene.add(pointLightCameraHelper)
 
-// DirectionalLightHelper
-// 用于模拟场景中平行光 DirectionalLight 的辅助对象. 其中包含了表示光位置的平面和表示光方向的线段.
-// DirectionalLightHelper( light : DirectionalLight, size : Number, color : Hex )
-// light-- 被模拟的光源.
-//     size -- (可选的) 平面的尺寸. 默认为 1.
-// color -- (可选的) 如果没有设置颜色将使用光源的颜色.
-const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
-scene.add(directionalLightHelper)
 
-// PointLightHelper
-// 创建一个虚拟的球形网格 Mesh 的辅助对象来模拟 点光源 PointLight.
+// CameraHelper
 // 构造函数
-// PointLightHelper( light : PointLight, sphereSize : Float, color : Hex )
-// light -- 要模拟的光源.sphereSize -- (可选的) 球形辅助对象的尺寸. 默认为 1.
-// color -- (可选的) 如果没有赋值辅助对象将使用光源的颜色.
-const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
-scene.add(pointLightHelper)
+// CameraHelper( camera : Camera )
+// camera -- 被模拟的相机.
+//     为指定相机创建一个新的相机辅助对象 CameraHelper .
+//     属性
+// 请到基类 LineSegments 页面查看公共属性.
+//     .camera : Camera
+// 被模拟的相机.
+//     .pointMap : Object
+// 包含用于模拟相机的点.
+//     .matrix : Object
+// 请参考相机的世界矩阵 camera.matrixWorld.
+//     .matrixAutoUpdate : Object
+// 请查看 Object3D.matrixAutoUpdate. 这里设置为 false 表示辅助对象 使用相机的 matrixWorld.
+const directionLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+directionLightCameraHelper.visible = false
+scene.add(directionLightCameraHelper)
 
-// SpotLightHelper
-// 用于模拟聚光灯 SpotLight 的锥形辅助对象.
-// 构造函数
-// SpotLightHelper( light : SpotLight, color : Hex )
-// light -- 被模拟的聚光灯 SpotLight
-//     .color -- (可选的) 如果没有赋值辅助对象将使用光源的颜色.
-const spotLightHelper = new THREE.SpotLightHelper(spotLight)
-scene.add(spotLightHelper)
-//这两个是连在一起用的 哈
-window.requestAnimationFrame(() => {
-    spotLightHelper.update()
-})
-
-// RectAreaLightHelper
-// 创建一个表示 RectAreaLight 的辅助对象.
-// 构造函数
-// RectAreaLightHelper( light : RectAreaLight, color : Hex )
-// light -- 被模拟的光源.color -- (可选) 如果没有赋值辅助对象将使用光源的颜色.属性
-// 请到基类 Object3D 页面查看公共属性..light : RectAreaLight
-// 被模拟的区域光源..color : hex
-// 构造函数中传入的颜色值. 默认为 undefined. 如果改变该值, 辅助对象的颜色将在下一次 update 被调用时更新.
-const rectAreaLightHelper = new RectAreaLightHelper(rectLight)
-scene.add(rectAreaLightHelper)
-
+/**
+ * Materials
+ */
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.7
+gui.add(material, 'metalness').min(0).max(1).step(0.001)
+gui.add(material, 'roughness').min(0).max(1).step(0.001)
 
 /**
  * Objects
  */
-// Material
-const material = new THREE.MeshStandardMaterial()
-material.roughness = 0.4
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
+)
 
-// Objects
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material)
-sphere.position.x = -1.5
+//.castShadow : Boolean
+// 对象是否被渲染到阴影贴图中。默认值为false。
+sphere.castShadow = true
 
-const cube = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), material)
-
-const torus = new THREE.Mesh(new THREE.TorusGeometry(0.3, 0.2, 32, 64), material)
-torus.position.x = 1.5
-
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material)
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(5, 5),
+    material
+)
 plane.rotation.x = -Math.PI * 0.5
-plane.position.y = -0.65
+plane.position.y = -0.5
 
-scene.add(sphere, cube, torus, plane)
+//.receiveShadow : Boolean
+// 材质是否接收阴影。默认值为false。
+plane.receiveShadow = true
+
+scene.add(sphere, plane)
+
+
+const sphereShaow = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5, 1.5),
+    new THREE.MeshBasicMaterial({
+        color: 0x000000,
+        transparent: true,
+        alphaMap: simpleShadow
+    })
+)
+sphereShaow.rotation.x = -Math.PI * 0.5
+sphereShaow.position.y = plane.position.y + 0.01
+scene.add(sphereShaow)
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth, height: window.innerHeight
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
 window.addEventListener('resize', () => {
@@ -202,8 +186,8 @@ window.addEventListener('resize', () => {
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
-camera.position.y = 2
-camera.position.z = 4
+camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
@@ -218,23 +202,48 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// renderer .shadowMap : WebGLShadowMap
+// 如果使用，它包含阴影贴图的引用。
+// - enabled: 如果设置开启，允许在场景中使用阴影贴图。默认是 false。
+// - autoUpdate: 启用场景中的阴影自动更新。默认是true
+// 如果不需要动态光照/阴影, 则可以在实例化渲染器时将之设为false
+// - needsUpdate: 当被设为true, 场景中的阴影贴图会在下次render调用时刷新。默认是false
+// 如果你已经禁用了阴影贴图的自动更新(shadowMap.autoUpdate = false), 那么想要在下一次渲染时更新阴影的话就需要将此值设为true
+// - type: 定义阴影贴图类型 (未过滤, 关闭部分过滤, 关闭部分双线性过滤), 可选值有:
+// THREE.BasicShadowMap
+// THREE.PCFShadowMap (默认)
+// THREE.PCFSoftShadowMap
+// THREE.VSMShadowMap
+// 详见Renderer constants
+renderer.shadowMap.enabled = false
+// 阴影类型
+// THREE.BasicShadowMap
+// THREE.PCFShadowMap
+// THREE.PCFSoftShadowMap
+// THREE.VSMShadowMap
+// 这些常量定义了WebGLRenderer中shadowMap.type的属性。
+// BasicShadowMap 能够给出没有经过过滤的阴影映射 —— 速度最快，但质量最差。
+// PCFShadowMap 为默认值，使用Percentage-Closer Filtering (PCF)算法来过滤阴影映射。
+// PCFSoftShadowMap 和PCFShadowMap一样使用 Percentage-Closer Filtering (PCF) 算法过滤阴影映射，但在使用低分辨率阴影图时具有更好的软阴影。
+// VSMShadowMap 使用Variance Shadow Map (VSM)算法来过滤阴影映射。当使用VSMShadowMap时，所有阴影接收者也将会投射阴影。
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
 
 /**
  * Animate
  */
 const clock = new THREE.Clock()
 
-const animate = () => {
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-    sphere.rotation.y = 0.1 * elapsedTime
-    cube.rotation.y = 0.1 * elapsedTime
-    torus.rotation.y = 0.1 * elapsedTime
+    sphere.position.x = Math.cos(elapsedTime)
+    sphere.position.z = Math.sin(elapsedTime)
+    sphere.position.y = Math.abs(Math.sin(elapsedTime * 3))
 
-    sphere.rotation.x = 0.15 * elapsedTime
-    cube.rotation.x = 0.15 * elapsedTime
-    torus.rotation.x = 0.15 * elapsedTime
+    sphereShaow.position.x = sphere.position.x
+    sphereShaow.position.z = sphere.position.z
+    sphereShaow.material.opacity = (1 - Math.abs(sphere.position.y)) * 0.3
 
     // Update controls
     controls.update()
@@ -243,7 +252,7 @@ const animate = () => {
     renderer.render(scene, camera)
 
     // Call tick again on the next frame
-    window.requestAnimationFrame(animate)
+    window.requestAnimationFrame(tick)
 }
 
-animate()
+tick()
