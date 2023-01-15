6309e1c861cbfcb9997afc5dcfcf244c64de9393
taixinF <yclzbb1349458861@163.com>
2023-01-15 23:05:56 +0800

    add post processing  30 28

--- src/app.js
+++ src/app.js
@@ -1,256 +1,186 @@
-import "./style.css";
-import * as THREE from "three";
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
-import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
-import * as dat from "dat.gui";
+import './style.css'
+import * as THREE from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
+//后期处理
+import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass.js'
+import {DotScreenPass} from 'three/examples/jsm/postprocessing/DotScreenPass.js'
+import {GlitchPass} from 'three/examples/jsm/postprocessing/GlitchPass.js'
+import {ShaderPass} from 'three/examples/jsm/postprocessing/ShaderPass.js'
+import {RGBShiftShader} from 'three/examples/jsm/shaders/RGBShiftShader.js'
+
+import * as dat from 'dat.gui'
+
 
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI();
+const gui = new dat.GUI()
 
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 /**
  * Loaders
  */
-const textureLoader = new THREE.TextureLoader();
-const gltfLoader = new GLTFLoader();
-const cubeTextureLoader = new THREE.CubeTextureLoader();
+const gltfLoader = new GLTFLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+const textureLoader = new THREE.TextureLoader()
 
 /**
  * Update all materials
  */
 const updateAllMaterials = () => {
     scene.traverse((child) => {
-        if (
-            child instanceof THREE.Mesh &&
-            child.material instanceof THREE.MeshStandardMaterial
-        ) {
-            child.material.envMapIntensity = 5;
-            child.material.needsUpdate = true;
-            child.castShadow = true;
-            child.receiveShadow = true;
+        if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+            child.material.envMapIntensity = 5
+            child.material.needsUpdate = true
+            child.castShadow = true
+            child.receiveShadow = true
         }
-    });
-};
+    })
+}
 
 /**
  * Environment map
  */
 const environmentMap = cubeTextureLoader.load([
-    "../static/textures/environmentMaps2/0/px.jpg",
-    "../static/textures/environmentMaps2/0/nx.jpg",
-    "../static/textures/environmentMaps2/0/py.jpg",
-    "../static/textures/environmentMaps2/0/ny.jpg",
-    "../static/textures/environmentMaps2/0/pz.jpg",
-    "../static/textures/environmentMaps2/0/nz.jpg",
-]);
-environmentMap.encoding = THREE.sRGBEncoding;
+    '../static/textures/environmentMaps/0/px.jpg',
+    '../static/textures/environmentMaps/0/nx.jpg',
+    '../static/textures/environmentMaps/0/py.jpg',
+    '../static/textures/environmentMaps/0/ny.jpg',
+    '../static/textures/environmentMaps/0/pz.jpg',
+    '../static/textures/environmentMaps/0/nz.jpg'
+])
+environmentMap.encoding = THREE.sRGBEncoding
 
-scene.background = environmentMap;
-scene.environment = environmentMap;
-
-/**
- * Material
- */
-
-// Textures
-const mapTexture = textureLoader.load(
-    "../static/models/LeePerrySmith/color.jpg"
-);
-mapTexture.encoding = THREE.sRGBEncoding;
-
-const normalTexture = textureLoader.load(
-    "../static/models/LeePerrySmith/normal.jpg"
-);
-
-// Material
-const material = new THREE.MeshStandardMaterial({
-    map: mapTexture,
-    normalMap: normalTexture,
-});
-
-const depthMaterial = new THREE.MeshDepthMaterial({
-    depthPacking: THREE.RGBADepthPacking
-})
-
-const customUniforms = {
-    uTime: {value: 0}
-}
-//在编译前 可以使用这个函数拿到材质的一些参数
-material.onBeforeCompile = (shader) => {
-    shader.uniforms.uTime = customUniforms.uTime;
-    shader.vertexShader = shader.vertexShader.replace(
-        "#include <common>",
-        `
-            #include <common>
-            uniform float uTime;
-            mat2 get2dRotateMatrix(float _angle)
-            {
-                return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
-            }
-        `
-    );
-
-    shader.vertexShader = shader.vertexShader.replace(
-        '#include <beginnormal_vertex>',
-        `
-            #include <beginnormal_vertex>
-             float angle = (sin(position.y + uTime)) * 0.4;
-            mat2 rotateMatrix = get2dRotateMatrix(angle);
-            objectNormal.xz = rotateMatrix * objectNormal.xz;
-        `
-    )
-
-    shader.vertexShader = shader.vertexShader.replace(
-        "#include <begin_vertex>",
-        `
-            #include <begin_vertex>
-            transformed.xz = rotateMatrix * transformed.xz;
-        `
-    );
-};
-
-depthMaterial.onBeforeCompile = (shader) => {
-    shader.uniforms.uTime = customUniforms.uTime;
-    shader.vertexShader = shader.vertexShader.replace(
-        "#include <common>",
-        `
-            #include <common>
-            uniform float uTime;
-            mat2 get2dRotateMatrix(float _angle)
-            {
-                return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
-            }
-        `
-    );
-    shader.vertexShader = shader.vertexShader.replace(
-        "#include <begin_vertex>",
-        `
-            #include <begin_vertex>
-            float angle = (sin(position.y + uTime)) * 0.4;
-            mat2 rotateMatrix = get2dRotateMatrix(angle);
-            transformed.xz = rotateMatrix * transformed.xz;
-        `
-    );
-}
+scene.background = environmentMap
+scene.environment = environmentMap
 
 /**
  * Models
  */
-gltfLoader.load("../static/models/LeePerrySmith/LeePerrySmith.glb", (gltf) => {
-    // Model
-    const mesh = gltf.scene.children[0];
-    mesh.rotation.y = Math.PI * 0.5;
-    mesh.material = material;
-    mesh.customDepthMaterial = depthMaterial
-    scene.add(mesh);
+gltfLoader.load(
+    '../static/models/DamagedHelmet/glTF/DamagedHelmet.gltf',
+    (gltf) => {
+        gltf.scene.scale.set(2, 2, 2)
+        gltf.scene.rotation.y = Math.PI * 0.5
+        scene.add(gltf.scene)
 
-    // Update materials
-    updateAllMaterials();
-});
-
-const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(15, 15, 15),
-    new THREE.MeshStandardMaterial()
+        updateAllMaterials()
+    }
 )
-plane.rotation.y = Math.PI
-plane.position.y = -5
-plane.position.z = 5
-scene.add(plane)
-
 
 /**
  * Lights
  */
-const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.set(1024, 1024);
-directionalLight.shadow.camera.far = 15;
-directionalLight.shadow.normalBias = 0.05;
-directionalLight.position.set(0.25, 2, -2.25);
-scene.add(directionalLight);
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.normalBias = 0.05
+directionalLight.position.set(0.25, 3, -2.25)
+scene.add(directionalLight)
 
 /**
  * Sizes
  */
 const sizes = {
     width: window.innerWidth,
-    height: window.innerHeight,
-};
+    height: window.innerHeight
+}
 
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
     // Update sizes
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
     // Update camera
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
     // Update renderer
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(
-    75,
-    sizes.width / sizes.height,
-    0.1,
-    100
-);
-camera.position.set(4, 1, -4);
-scene.add(camera);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(4, 1, -4)
+scene.add(camera)
 
 // Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
-    antialias: true,
-});
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFShadowMap;
-renderer.physicallyCorrectLights = true;
-renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 1;
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    antialias: true
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFShadowMap
+renderer.physicallyCorrectLights = true
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.toneMapping = THREE.ReinhardToneMapping
+renderer.toneMappingExposure = 1.5
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/*
+* Post processing
+* */
+const effectComposer = new EffectComposer(renderer)
+//.setPixelRatio ( pixelRatio : Float ) : undefined
+// pixelRatio -- 设备像素比
+effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+effectComposer.setSize(sizes.width, sizes.height)
+
+const renderPass = new RenderPass(scene, camera)
+effectComposer.addPass(renderPass)
+
+const dotScreenPass = new DotScreenPass()
+dotScreenPass.enabled = false
+effectComposer.addPass(dotScreenPass)
+
+const glitchPass = new GlitchPass()
+glitchPass.goWild = false
+glitchPass.enabled = false
+effectComposer.addPass(glitchPass)
+console.log(effectComposer)
+
+const rgbShiftPass = new ShaderPass(RGBShiftShader)
+rgbShiftPass.enabled = false
+effectComposer.addPass(rgbShiftPass)
 
 /**
  * Animate
  */
-const clock = new THREE.Clock();
+const clock = new THREE.Clock()
 
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
-
-    // Update material
-    customUniforms.uTime.value = elapsedTime
+    const elapsedTime = clock.getElapsedTime()
 
     // Update controls
-    controls.update();
+    controls.update()
 
     // Render
-    renderer.render(scene, camera);
+    // renderer.render(scene, camera)
+    //后期处理使用这个函数调用render
+    effectComposer.render()
 
     // Call tick again on the next frame
-    window.requestAnimationFrame(tick);
-};
+    window.requestAnimationFrame(tick)
+}
 
-tick();
+tick()
